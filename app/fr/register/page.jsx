3eb468fc363995330c35aa5665eb2b5f626b1177
Roma <pochtmanrca@gmail.com>
2025-10-07 20:10:52 +0200

    asadad

--- app/fr/register/page.jsx
+++ app/fr/register/page.jsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
 import Register from '../../../src/components/Register';
+import Loading from '../../../src/components/Loading';
 
 export default function FrenchRegisterPage() {
   return (
     <div dir="ltr" lang="fr">
-      <Register />
+      <Suspense fallback={<Loading />}>
+        <Register />
+      </Suspense>
     </div>
   );
 }
