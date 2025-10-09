d4dd5a5e433339e16ab2e077972bc60662227d05
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2025-10-09 10:42:57 +0300

    blog

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,8 +1,8 @@
-import { Suspense } from 'react'
-import Script from 'next/script'
-import Blog from '../../src/components/Blog'
-import Loading from '../../src/components/Loading'
-import RTLWrapper from '../../src/components/RTLWrapper'
+import { Suspense } from 'react';
+import Script from 'next/script';
+import Blog from '../../src/components/Blog';
+import Loading from '../../src/components/Loading';
+import RTLWrapper from '../../src/components/RTLWrapper';
 
 export const metadata = {
   title: 'Blog - eSIM Plans',
@@ -26,6 +26,18 @@ export default function BlogPage() {
           <Blog />
         </Suspense>
       </RTLWrapper>
+      
+      {/* AppsFlyer Banner SDK */}
+      <Script
+        id="appsflyer-sdk"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AF||function(){(t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))},t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],o.async=1,o.src="https://websdk.appsflyer.com?"+(c.length>0?"st="+c.split(",").sort().join(",")+"&":"")+(i.length>0?"af_id="+i:""),p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "ae5b205a-0713-453d-adf9-4ec72aebdea7"}});
+            AF('banners', 'showBanner')
+          `
+        }}
+      />
     </>
   )
 }
