6e8c2173369af425f149ab19b3ada9720ba2edfa
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2025-10-09 10:17:50 +0300

    rest

--- app/de/blog/page.jsx
+++ app/de/blog/page.jsx
@@ -32,16 +32,7 @@ export default function GermanBlogPage() {
         dangerouslySetInnerHTML={{
           __html: `
             !function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AF||function(){(t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))},t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],o.async=1,o.src="https://websdk.appsflyer.com?"+(c.length>0?"st="+c.split(",").sort().join(",")+"&":"")+(i.length>0?"af_id="+i:""),p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "ae5b205a-0713-453d-adf9-4ec72aebdea7"}});
-            
-            // Wait for SDK to load before showing banner
-            setTimeout(function() {
-              if (window.AF) {
-                console.log('AppsFlyer SDK loaded, showing banner');
-                AF('banners', 'showBanner');
-              } else {
-                console.warn('AppsFlyer SDK not loaded yet');
-              }
-            }, 1000);
+            AF('banners', 'showBanner')
           `
         }}
       />
