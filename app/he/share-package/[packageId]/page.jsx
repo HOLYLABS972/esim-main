01d729578511a19be54282d12a5fbfa858fe3450
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2025-10-13 03:38:38 +0300

    deploy

--- app/he/share-package/[packageId]/page.jsx
+++ app/he/share-package/[packageId]/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React from 'react';
-import SharePackagePage from '../../../../share-package/[packageId]/page';
+import SharePackagePage from '../../../share-package/[packageId]/page';
 
 // Hebrew version - uses the same component as English
 export default SharePackagePage;
--- app/ru/share-package/[packageId]/page.jsx
+++ app/ru/share-package/[packageId]/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React from 'react';
-import SharePackagePage from '../../../../share-package/[packageId]/page';
+import SharePackagePage from '../../../share-package/[packageId]/page';
 
 // Russian version - uses the same component as English
 export default SharePackagePage;
