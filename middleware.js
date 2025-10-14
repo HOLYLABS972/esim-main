8b3ccf87bcf663844a2a042c6f4f3d1f9fb0fbb1
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2025-10-14 16:37:31 +0300

    respecting issue

--- middleware.js
+++ middleware.js
@@ -2,16 +2,16 @@ import { NextResponse } from 'next/server';
 
 // Domain to language mapping
 const domainLanguageMap = {
-  'ru.romajet.net': 'ru',
-  'esim.romajet.net': 'en',
-  'www.romajet.net': 'en',
-  'romajet.net': 'en',
+  'ru.roamjet.net': 'ru',
+  'esim.roamjet.net': 'en',
+  'www.roamjet.net': 'en',
+  'roamjet.net': 'en',
   // Add more domains as needed
-  'ar.romajet.net': 'ar',
-  'he.romajet.net': 'he',
-  'de.romajet.net': 'de',
-  'fr.romajet.net': 'fr',
-  'es.romajet.net': 'es',
+  'ar.roamjet.net': 'ar',
+  'he.roamjet.net': 'he',
+  'de.roamjet.net': 'de',
+  'fr.roamjet.net': 'fr',
+  'es.roamjet.net': 'es',
 };
 
 const supportedLanguageCodes = ['en', 'es', 'fr', 'de', 'ar', 'he', 'ru'];
