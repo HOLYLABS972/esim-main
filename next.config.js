f21a256699562f5f80bde9a5699a297f05683b19
Holylabs <dima@holylabs.net>
2026-02-10 19:32:26 +0000

    fix: add images.pexels.com to allowed image domains for blog

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com', 'localhost'],
+    domains: ['firebasestorage.googleapis.com', 'localhost', 'images.pexels.com'],
     formats: ['image/webp', 'image/avif'],
   },
   trailingSlash: false,
