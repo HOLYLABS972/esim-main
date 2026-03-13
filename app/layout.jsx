87d9b9bdeb2d5b97f667585e10ea939f55daff21
AAAxis <dima@holylabs.net>
2026-03-13 08:34:14 +0000

    Replace custom Trustpilot banner with official TrustBox widget

--- app/layout.jsx
+++ app/layout.jsx
@@ -110,6 +110,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* TrustBox script */}
+        <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17231669358"></script>
         <script
